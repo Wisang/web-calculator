e6cee12e2a9f88f040253e6783889550616cf572
wisang <wseom7@gmail.com>
2022-09-01 18:02:49 +0900

    completed

--- calc.js
+++ calc.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  var number1 = Number(req.body.num1);
+  var number2 = Number(req.body.num2);
+  var result = number1 + number2;
+  res.send("The result is " + result);
 });
 
 app.listen(3000);
